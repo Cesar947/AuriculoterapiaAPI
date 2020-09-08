0e7a8ebd80a6c9a36017eb6c8c2778ca44d895a3
Cesar947 <u201710947@upc.edu.pe>
2020-09-07 23:57:34 -0500

    Validar que al solicitar en un horario en donde no haya sido registrada una disponibilidad, se registre una

--- Service/Implementation/CitaService.cs
+++ Service/Implementation/CitaService.cs
@@ -12,17 +12,20 @@ namespace Auriculoterapia.Api.Service.Implementation
         private readonly IPacienteRepository PacienteRepository;
         private readonly ITipoAtencionRepository tipoAtencionRepository;
 
+        private readonly IEspecialistaRepository especialistaRepository;
         private readonly IDisponibilidadRepository disponibilidadRepository;
         private IHorarioDescartadoRepository horarioDescartadoRepository;
     
 
         public CitaService(ICitaRepository CitaRepository, IPacienteRepository PacienteRepository, ITipoAtencionRepository tipoAtencionRepository,
-        IDisponibilidadRepository disponibilidadRepository, IHorarioDescartadoRepository horarioDescartadoRepository){
+        IDisponibilidadRepository disponibilidadRepository, IHorarioDescartadoRepository horarioDescartadoRepository,
+        IEspecialistaRepository especialistaRepository){
             this.CitaRepository = CitaRepository;
             this.PacienteRepository = PacienteRepository;
             this.tipoAtencionRepository = tipoAtencionRepository;
             this.disponibilidadRepository = disponibilidadRepository;
             this.horarioDescartadoRepository = horarioDescartadoRepository;
+            this.especialistaRepository = especialistaRepository;
       
         }
 
@@ -34,34 +37,50 @@ namespace Auriculoterapia.Api.Service.Implementation
             var cita = new Cita();
             var conversor = new ConversorDeFechaYHora(); 
              var disponibilidad = new Disponibilidad();
-            try {
-                var tipoAtencion = tipoAtencionRepository.FindByDescription(entity.TipoAtencion);
-                var paciente = PacienteRepository.FindById(PacienteId);
-                cita.Fecha = conversor.TransformarAFecha(entity.Fecha);
-                cita.HoraInicioAtencion = conversor.TransformarAHora(entity.HoraInicioAtencion, entity.Fecha);
-                cita.HoraFinAtencion = conversor.TransformarAHora(entity.HoraFinAtencion, entity.Fecha);
-                cita.Estado = "En Proceso";
-                cita.PacienteId = paciente.Id;
-                cita.Paciente = paciente;
-                cita.TipoAtencionId = tipoAtencion.Id;
-                cita.TipoAtencion = tipoAtencion;
-                Save(cita);
-
-                disponibilidad = disponibilidadRepository.listarPorFecha(entity.Fecha);
+                try {
+                    var tipoAtencion = tipoAtencionRepository.FindByDescription(entity.TipoAtencion);
+                    var paciente = PacienteRepository.FindById(PacienteId);
+                    cita.Fecha = conversor.TransformarAFecha(entity.Fecha);
+                    cita.HoraInicioAtencion = conversor.TransformarAHora(entity.HoraInicioAtencion, entity.Fecha);
+                    cita.HoraFinAtencion = conversor.TransformarAHora(entity.HoraFinAtencion, entity.Fecha);
+                    cita.Estado = "En Proceso";
+                    cita.PacienteId = paciente.Id;
+                    cita.Paciente = paciente;
+                    cita.TipoAtencionId = tipoAtencion.Id;
+                    cita.TipoAtencion = tipoAtencion;
+                    Save(cita);
                 
+                    var horarioDescartado = new HorarioDescartado();
+                        horarioDescartado.HoraInicio = cita.HoraInicioAtencion;
+                        horarioDescartado.HoraFin = cita.HoraFinAtencion;
+                        
+                    disponibilidad = disponibilidadRepository.listarPorFecha(entity.Fecha);
+                    
+                    if(disponibilidad != null){
+                        
+                            horarioDescartado.DisponibilidadId = disponibilidad.Id;
+                            horarioDescartado.Disponibilidad = disponibilidad;
 
-                var horarioDescartado = new HorarioDescartado();
-                    horarioDescartado.HoraInicio = cita.HoraInicioAtencion;
-                    horarioDescartado.HoraFin = cita.HoraFinAtencion;
-                    horarioDescartado.DisponibilidadId = disponibilidad.Id;
-                    horarioDescartado.Disponibilidad = disponibilidad;
-                
-                this.horarioDescartadoRepository.Save(horarioDescartado);
+                            
+                    } else {
+                        disponibilidad = new Disponibilidad();
+                        disponibilidad.Dia = cita.Fecha;
+                        disponibilidad.HoraInicio = conversor.TransformarAHora("07:00", entity.Fecha);
+                        disponibilidad.HoraFin = conversor.TransformarAHora("19:00", entity.Fecha);
+                        disponibilidad.EspecialistaId = 1;
+                        disponibilidad.Especialista = this.especialistaRepository.FindById(disponibilidad.EspecialistaId);
+                        var disInserted = this.disponibilidadRepository.guardarDisponibilidad(disponibilidad); 
 
-            }catch(System.Exception){
+                        horarioDescartado.DisponibilidadId = disInserted.Id;
+                        horarioDescartado.Disponibilidad = disInserted;
+                    }
 
-                throw;
-            }
+                    this.horarioDescartadoRepository.Save(horarioDescartado);
+
+                }catch(System.Exception){
+
+                    throw;
+                }
         }
 
         public void RegistrarCitaPaciente(FormularioCitaPaciente entity, int PacienteId){
@@ -82,15 +101,32 @@ namespace Auriculoterapia.Api.Service.Implementation
                 cita.TipoAtencionId = tipoAtencion.Id;
                 cita.TipoAtencion = tipoAtencion;
                 Save(cita);
-                disponibilidad = disponibilidadRepository.listarPorFecha(entity.Fecha);
-                
 
                 var horarioDescartado = new HorarioDescartado();
-                    horarioDescartado.HoraInicio = cita.HoraInicioAtencion;
-                    horarioDescartado.HoraFin = cita.HoraFinAtencion;
-                    horarioDescartado.DisponibilidadId = disponibilidad.Id;
-                    horarioDescartado.Disponibilidad = disponibilidad;
+                horarioDescartado.HoraInicio = cita.HoraInicioAtencion;
+                horarioDescartado.HoraFin = cita.HoraFinAtencion;
+
+                disponibilidad = disponibilidadRepository.listarPorFecha(entity.Fecha);
                 
+                if(disponibilidad != null){
+                      
+                        horarioDescartado.DisponibilidadId = disponibilidad.Id;
+                        horarioDescartado.Disponibilidad = disponibilidad;
+
+                        
+                } else {
+                    disponibilidad = new Disponibilidad();
+                    disponibilidad.Dia = cita.Fecha;
+                    disponibilidad.HoraInicio = conversor.TransformarAHora("07:00", entity.Fecha);
+                    disponibilidad.HoraFin = conversor.TransformarAHora("19:00", entity.Fecha);
+                    disponibilidad.EspecialistaId = 1;
+                    disponibilidad.Especialista = this.especialistaRepository.FindById(disponibilidad.EspecialistaId);
+                    var disInserted = this.disponibilidadRepository.guardarDisponibilidad(disponibilidad); 
+
+                    horarioDescartado.DisponibilidadId = disInserted.Id;
+                    horarioDescartado.Disponibilidad = disInserted;
+                }
+
                 this.horarioDescartadoRepository.Save(horarioDescartado);
                 
 
@@ -100,10 +136,15 @@ namespace Auriculoterapia.Api.Service.Implementation
             }
         }
 
+        public void procesarHorariosDescartados(Cita cita, FormularioCita fs=null, FormularioCitaPaciente fp=null){
+
+
+        }
+
         public IEnumerable<Cita> FindAll(){
            return CitaRepository.FindAll();
         }
-
+    
         public IEnumerable<Cita> listarCitasPorUsuarioId(int usuarioId){
             return CitaRepository.listarCitasPorUsuarioId(usuarioId);
         }
