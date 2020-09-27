e095699e7123687704bb5922d9473fe877e7b325
Cesar947 <u201710947@upc.edu.pe>
2020-09-27 16:50:17 -0500

    edad nula

--- Migrations/20200927214755_edadNula.Designer.cs
+++ Migrations/20200927214755_edadNula.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Auriculoterapia.Api.Repository.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Auriculoterapia.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200927214755_edadNula")]
+    partial class edadNula
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
